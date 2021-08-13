0d2aef6909ca4e8e548933ac4e9c3b08e8983e57
Elena Lushnikova <elushnikova.256@gmail.com>
2021-08-13 15:10:49 +0300

    fix: Create session on login

--- backend/controllers/login.js
+++ backend/controllers/login.js
@@ -5,11 +5,16 @@ const prepareUserProfile = require('../helpers/prepareUserProfile');
 function login(req, res) {
   prepareLoginData(req.body.email, req.body.password)
     .then(compareUser)
-    .then(([user, isMatch]) => (
-      isMatch
-        ? res.json({ ok: true, profile: prepareUserProfile(user) })
-        : res.status(401).json({ ok: false, message: 'Invalid email or password' })
-    ))
+    .then(([user, isMatch]) => {
+      if (!isMatch) {
+        res.status(401).json({ ok: false, message: 'Invalid email or password' });
+        return;
+      }
+
+      const profile = prepareUserProfile(user);
+      req.session.profile = profile;
+      res.json({ ok: true, profile });
+    })
     .catch(() => {
       res.status(401).json({ ok: false, message: 'Invalid email or password' });
     });
