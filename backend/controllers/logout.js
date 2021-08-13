f9befc17a29c329986f8b44a1d879153bf8f7047
Elena Lushnikova <elushnikova.256@gmail.com>
2021-08-13 12:34:53 +0300

    fix(logout): Handle logout error

--- backend/controllers/logout.js
+++ backend/controllers/logout.js
@@ -1,10 +1,17 @@
 const SESSION_COOKIE = process.env.SESSION_COOKIE || 'user_sid';
 
 function logout(req, res) {
-  req.session.destroy();
-  res
-    .clearCookie(SESSION_COOKIE)
-    .json({ ok: true });
+  req.session.destroy((error) => {
+    if (error) {
+      return res
+        .status(500)
+        .json({ ok: false, message: 'Error logging out' });
+    }
+
+    return res
+      .clearCookie(SESSION_COOKIE)
+      .json({ ok: true });
+  });
 }
 
 module.exports = logout;
