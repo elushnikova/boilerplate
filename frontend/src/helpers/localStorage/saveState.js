14d403050aefbb44372aa777805361437c4db072
Elena Lushnikova <elushnikova.256@gmail.com>
2021-08-25 12:27:16 +0300

    refactor(saveState): Verify logic, add docs

--- frontend/src/helpers/localStorage/saveState.js
+++ frontend/src/helpers/localStorage/saveState.js
@@ -1,9 +1,17 @@
+/**
+ * Сохранить данные в JSON-формате в local storage.
+ * @param {string} key Ключ для хранения.
+ * @param {*} state Данные для хранения.
+ * @todo Переименовать функцию/параметр `state`/переменные?
+ */
 function saveState(key, state) {
+  const serializedState = JSON.stringify(state);
+
   try {
-    const serializedState = JSON.stringify(state);
     localStorage.setItem(key, serializedState);
   } catch (error) {
-    console.log(error); // eslint-disable-line no-console
+    // eslint-disable-next-line no-console
+    console.error('Failed to save data to local storage');
   }
 }
 
